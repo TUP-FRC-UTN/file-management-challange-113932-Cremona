ec6a7792153b7d01e708755349cf83d2ddfa4e98
113932_Cremona_Juan <113932@tecnicatura.frc.utn.edu.ar>
2024-09-11 22:01:51 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FileItem } from '../models/file.item.model';
+import { FileItem, FileType } from '../models/file.item.model';
 import { FILE_LIST } from '../data/file.storage';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   files: FileItem[] = FILE_LIST;
   title = 'file-management';
+
+  folderList: FileItem[] =[]
+  fileList: FileItem[]=[]
+
+  
+  ngOnInit(){
+    
+    this.folderList = this.files
+      .filter(file => file.type === FileType.FOLDER)
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    // Filtrar y ordenar archivos (FILES)
+    this.fileList = this.files
+      .filter(file => file.type === FileType.FILE)
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+    this.files = this.folderList
+    for(let i = 0; i < this.fileList.length;i++){
+      this.files.push(this.fileList[i]);
+    }
+
+  }
+  
+
+  
 }
